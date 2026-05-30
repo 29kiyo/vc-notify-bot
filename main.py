cd0f116b74d08f4048fa54120e698efed33c0c87
29kiyo <148545965+29kiyo@users.noreply.github.com>
2026-05-30 14:44:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -489,26 +489,6 @@ async def listenerlist(interaction: discord.Interaction):
         ephemeral=True
     )
 
-# =========================
-# /debug-members
-# =========================
-
-@bot.tree.command(
-    name="debug-members",
-    description="取得できるメンバー確認"
-)
-async def debug_members(interaction: discord.Interaction):
-
-    members = [
-        m.display_name
-        for m in interaction.guild.members
-    ]
-
-    await interaction.response.send_message(
-        f"取得人数: {len(members)}\n"
-        + "\n".join(members[:20]),
-        ephemeral=True
-    )
 
 # =========================
 # /admin-setchannel
